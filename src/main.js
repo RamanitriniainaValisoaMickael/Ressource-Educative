91cd414cd160d9adb0e06cf5c2eb71ae76e13ee5
mika2764 <ramanitriniainavalisoamickael@gmail.com>
2025-03-19 16:55:21 +0300

    commit faha

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { createHead } from '@vueuse/head';
 
+const head=createHead();
 createApp(App)
   .use(router)
-  .mount('#app');
+  .mount('#app')
+  .use(head);
