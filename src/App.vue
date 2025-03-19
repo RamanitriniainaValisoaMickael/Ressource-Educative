bc42ea320a262923643e02049a804ab839a71ed8
mika2764 <ramanitriniainavalisoamickael@gmail.com>
2025-03-19 16:26:54 +0300

    commit fahaefatra

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="app">
+  <RouterView></RouterView>
+  <!-- <div id="app">
     <nav>
-      <router-link to="/">Accueil</router-link> |
-      <router-link to="/cours">Cours</router-link> |
-      <router-link to="/bibliographie">Bibliographie</router-link> |
-      <router-link to="/glossaire">Glossaire</router-link> |
+      <router-link to="/accueil">Accueil</router-link> | 
+      <router-link to="/cours">Cours</router-link> | 
+      <router-link to="/bibliographie">Bibliographie</router-link> | 
+      <router-link to="/glossaire">Glossaire</router-link> | 
       <router-link to="/contact">Contact</router-link>
     </nav>
     <router-view></router-view>
-  </div>
+  </div> -->
 </template>
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
 
 <style>
 nav {
@@ -17,13 +21,11 @@ nav {
   text-align: center;
   background-color: #f0f0f0;
 }
-
 nav a {
   margin: 0 10px;
   text-decoration: none;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
   font-weight: bold;
